89ecb49504a35a780e43477036682866aa212686
Isaac Zolana <hizackdavid@hotmail.com>
2025-01-01 19:42:56 -0500

    update alt value

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,10 +15,15 @@ const Navbar = () => {
       {/* ICONS AND USER */}
       <div className="flex items-center gap-6 justify-end w-full">
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
-          <Image src="/message.png" alt="" width={20} height={20} />
+          <Image src="/message.png" alt="message" width={20} height={20} />
         </div>
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
-          <Image src="/announcement.png" alt="" width={20} height={20} />
+          <Image
+            src="/announcement.png"
+            alt="announcement"
+            width={20}
+            height={20}
+          />
           <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">
             1
           </div>
